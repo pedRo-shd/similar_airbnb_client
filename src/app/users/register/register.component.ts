e62e5a59846c358321b66a2042eab3c1e31e4746
pedRo-shd <pedrocustodio@outlook.com.br>
2017-01-24 10:30:32 -0200

    M1-Autenticação, Integração, Autorização, Header, Footer e Home

---
+++ src/app/users/register/register.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit} from '@angular/core';
+import { Angular2TokenService, RegisterData } from 'angular2-token';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  private _registerData: RegisterData = <RegisterData>{};
+  private _output: any;
+
+  constructor(private _tokenService: Angular2TokenService) { }
+
+  ngOnInit() {
+  }
+
+  // Submit Data to Backend
+  onSubmit() {
+
+    this._output = null;
+
+    this._tokenService.registerAccount(this._registerData).subscribe(
+      res => {
+        this._registerData = <RegisterData>{};
+        this._output = res;
+        console.log('res');
+        console.log(res);
+        console.log('registerData');
+        console.log(this._registerData);
+      }, error => {
+        this._registerData = <RegisterData>{};
+        this._output = error;
+      }
+    );
+  }
+
+}
